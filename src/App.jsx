39def9cd47ce13ebae4003f16e97358e294419b5
KevinJp21 <kevinjp821@gmail.com>
2024-04-14 18:30:18 -0500

    se agregó el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import CardContainer from './Components/Cards/CardContainer';
 import './App.css'
-
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <CardContainer />
     </>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg" id="navbar">
+      <div className="container-fluid" id="container-fluid">
+        <a className="navbar-brand fs-2" href="#" id="logo">
+          Restaurante S <span className="yellow fs-2">Co</span>
+          <span className=" blue fs-2">ci</span>
+          <span className="red fs-2">na</span>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span>
+            <i className="fa-solid fa-bars fs-1 bars"></i>
+          </span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav ms-auto mb-2 mb-lg-0 navcontent">
+              <a className="nav-link fs-5 " aria-current="page" href="#home">
+                Inicio
+              </a>
+              <a className="nav-link fs-5" href="#about">
+                Sobre nosotros
+              </a>
+              <a className="nav-link fs-5" href="#menu">
+                Menú
+              </a>
+              <a className="nav-link fs-5" href="#contact">
+                Contactanos
+              </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
