83eaee151cc12af3bfd42d209b592a45ba743bff
KevinJp21 <kevinjp821@gmail.com>
2024-04-16 12:56:35 -0500

    se terminó la sección home

--- src/Components/Home/HomeS.jsx
+++ src/Components/Home/HomeS.jsx
@@ -14,9 +14,21 @@ const Home = () => {
                 data.filter(data => data.id === "9").map((data) => (
                   <section id="home" key={data.id}
                   style={{
-                    backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${data.img})`,
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.1)), url(${data.img})`,
                   }}
-                  ></section>
+                  >
+                    <div className="content" id="content">
+                        <h3>
+                          Mejora tu día con 
+                          <br />
+                          comida 100% Colombiana
+                        </h3>
+                        <p>
+                        Explora la diversidad de sabores de Colombia a través de nuestros platos, que te llevarán desde las montañas de los Andes hasta las costas del Caribe.
+                        </p>
+                        <button className='btn btn-buy' id="btn-home">Pide ahora!</button>
+                    </div>
+                  </section>
                 ))
               ) : (
                 <p>No hay datos disponibles.</p>
