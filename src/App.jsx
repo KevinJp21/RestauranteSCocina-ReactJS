3dabaf6ff4a62d316eb26c99b71c3f29b542c5a7
KevinJp21 <kevinjp821@gmail.com>
2024-04-16 14:47:47 -0500

    seccion about us finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css'
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/HomeS';
-import CardContainer from './Components/Cards/CardContainer';
+import About from './Components/About/About';
+import CardContainer from './Components/Menu/CardContainer';
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
     <>
       <NavBar />
       <Home />
+      <About />
       <CardContainer />
     </>
   );
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './About.css'
+import ProductData from '../../ProductsData';
+
+const About = () => {
+  return (
+    <section id="about">
+        <div className="container" id="container">
+            <h2 id="heading">Sobre <span>nosotros</span></h2>
+        </div>
+        <div className="row" id="container-about">
+            <div className="col-lg-6" id="about-img">
+                <div className="img-about">
+                <ProductData
+        url="http://localhost/API-Foods/getFood.php"
+        render={(data) => {
+          return (
+            <>
+             {data.length > 0 ? (
+                // Filtrar para encontrar el item con ID=9
+                data.filter(data => data.id === "7").map((data) => (
+                    <img key={data.id} src={data.img} alt=''/>
+                ))
+              ) : (
+                <p>No hay datos disponibles.</p>
+              )}
+            </>
+          );
+        }}
+      />
+                </div>
+            </div>
+            <div className="col-lg-6" id="about-text">
+                <h3>¿Qué hace a nuestra comida especial?</h3>
+                <p>Cada región de Colombia tiene su propia identidad culinaria, influenciada por su clima, geografía y tradiciones. Desde los platos andinos con influencias indígenas hasta la cocina costeña con sabores del mar, hay una gran variedad de platos para explorar.
+                    <br />
+                    La cocina colombiana ha sido influenciada por diversas culturas a lo largo de su historia, incluyendo la indígena, española y africana. Estas influencias se reflejan en la variedad de ingredientes, técnicas de cocina y platos que se encuentran en todo el país.
+                </p>
+                <button className='btn' id='btn-about'>Saber más</button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
