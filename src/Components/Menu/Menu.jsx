75cc2b6d2ae98a420201bf3171b57d646816f7d3
KevinJp21 <kevinjp821@gmail.com>
2024-04-18 14:19:19 -0500

    Se actualizó la paginacion para que muestre el numero de paginas de acuerdo al filtro seleccionado

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,19 +5,18 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { MDBContainer } from "mdb-react-ui-kit";
 import DiscreteSlider from "./DiscreteSlider";
-import Pagination from "@mui/material/Pagination"; 
+import Pagination from "@mui/material/Pagination";
 
 const Menu = () => {
   const [category, setCategory] = useState('');
   const [maxPrice, setMaxPrice] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(6);  // Ajustar numero items por pagina
+  const [itemsPerPage, setItemsPerPage] = useState(6);  // Se establece el numero de items por pagina
 
   const handlePriceChange = (event, newValue) => {
     setMaxPrice(newValue);
   };
 
-  // Función para cambiar de página
   const handleChangePage = (event, newPage) => {
     setCurrentPage(newPage);
   };
@@ -31,11 +30,11 @@ const Menu = () => {
         <div className="filterDish" id="filterDish">
           <div className="filterPrice" id="filterPrice">
             <h4 className="fs-4 m-0">Precio a partir de:</h4>
-            <DiscreteSlider id="slider-content" value={maxPrice} onChange={handlePriceChange} />
+            <DiscreteSlider id="slider-content" value={maxPrice} onChange={handlePriceChange} /> {/*Slider de precios*/}
           </div>
           <div className="filterCategory" id="filterCategory">
-            <h4 className="fs-4 m-0">Categoria:</h4>
-            <select className="selectCategory" id="selectCategory" value={category} onChange={e => setCategory(e.target.value)}>
+            <h4 className="fs-4 m-0">Categoría:</h4>
+            <select className="selectCategory" id="selectCategory" value={category} onChange={e => setCategory(e.target.value)}>{/*Con el onChange, siempre está a la espera de una accion para establecer el valor de forma dinamica*/}
               <option value="">Todas las categorías</option>
               <option value="Plato principal">Plato principal</option>
               <option value="Entrada">Entrada</option>
@@ -49,29 +48,31 @@ const Menu = () => {
         <div className="card-container">
           <ProductData
             url="https://hoteless-hilton.000webhostapp.com/getFood.php"
-            render={(data) => (
-              <>
-                {data.length > 0 ? (
-                  <>
-                    {data
-                      .filter(dish => dish.categoria === category || category === '')
-                      .filter(dish => !maxPrice || Number(dish.precio) <= Number(maxPrice))
-                      .sort((a, b) => maxPrice ? Number(a.precio) - Number(b.precio) : 0)
-                      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-                      .map(dish => <Cards key={dish.id} data={dish} />)
-                    }
-                    <Pagination className="mt-4"
-                      count={Math.ceil(data.length / itemsPerPage)}
-                      page={currentPage}
-                      onChange={handleChangePage}
-                      color="primary"
-                    />
-                  </>
-                ) : (
-                  <p>No hay datos disponibles.</p>
-                )}
-              </>
-            )}
+            render={(data) => {
+              // Aplicar filtros antes de renderizar
+              const filteredData = data.filter(dish => (dish.categoria === category || category === '') && (!maxPrice || Number(dish.precio) <= Number(maxPrice)));
+              return (
+                <>
+                  {filteredData.length > 0 ? (
+                    <>
+                      {filteredData
+                        .sort((a, b) => maxPrice ? Number(a.precio) - Number(b.precio) : 0)/*ordenar items de menor a mayor despues de seleccionar el filtro*/
+                        .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)/*Toma 2 argumentos, el de inicio y el final */
+                        .map(dish => <Cards key={dish.id} data={dish} />)/*Renderizarlos datos */
+                      }
+                      <Pagination className="mt-4"
+                        count={Math.ceil(filteredData.length / itemsPerPage)}
+                        page={currentPage}
+                        onChange={handleChangePage}
+                        color="primary"
+                      />
+                    </>
+                  ) : (
+                    <p>No hay datos disponibles.</p>
+                  )}
+                </>
+              );
+            }}
           />
         </div>
       </MDBContainer>
