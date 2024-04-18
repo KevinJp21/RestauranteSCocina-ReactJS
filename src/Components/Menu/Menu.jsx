cefcce40521e5c9425b806011507a6790090cd75
KevinJp21 <kevinjp821@gmail.com>
2024-04-18 14:04:51 -0500

    se agregó paginacion en el menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,13 +5,21 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { MDBContainer } from "mdb-react-ui-kit";
 import DiscreteSlider from "./DiscreteSlider";
+import Pagination from "@mui/material/Pagination"; 
 
 const Menu = () => {
-  const [category, setCategory] = useState('');  // Estado para la categoría seleccionada
-  const [maxPrice, setMaxPrice] = useState(0);  // Estado inicial para el precio máximo, ajustado al máximo del Slider
+  const [category, setCategory] = useState('');
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(6);  // Ajustar numero items por pagina
 
   const handlePriceChange = (event, newValue) => {
     setMaxPrice(newValue);
+  };
+
+  // Función para cambiar de página
+  const handleChangePage = (event, newPage) => {
+    setCurrentPage(newPage);
   };
 
   return (
@@ -21,20 +29,19 @@ const Menu = () => {
           <h2 id="heading">Nuestro <span>menú</span></h2>
         </div>
         <div className="filterDish" id="filterDish">
-          {/* Componente Slider para seleccionar el precio máximo */}
           <div className="filterPrice" id="filterPrice">
-          <h4 className="fs-4m-0">Precio a partir de:</h4>
-          <DiscreteSlider id="slider-content" value={maxPrice} onChange={handlePriceChange} />
+            <h4 className="fs-4 m-0">Precio a partir de:</h4>
+            <DiscreteSlider id="slider-content" value={maxPrice} onChange={handlePriceChange} />
           </div>
           <div className="filterCategory" id="filterCategory">
             <h4 className="fs-4 m-0">Categoria:</h4>
-          <select className="selectCategory" id="selectCategory" value={category} onChange={e => setCategory(e.target.value)}>
-            <option value="">Todas las categorías</option>
-            <option value="Plato principal">Plato principal</option>
-            <option value="Entrada">Entrada</option>
-            <option value="Bebida">Bebida</option>
-            <option value="Postre">Postre</option>
-          </select>
+            <select className="selectCategory" id="selectCategory" value={category} onChange={e => setCategory(e.target.value)}>
+              <option value="">Todas las categorías</option>
+              <option value="Plato principal">Plato principal</option>
+              <option value="Entrada">Entrada</option>
+              <option value="Bebida">Bebida</option>
+              <option value="Postre">Postre</option>
+            </select>
           </div>
         </div>
       </div>
@@ -45,11 +52,21 @@ const Menu = () => {
             render={(data) => (
               <>
                 {data.length > 0 ? (
-                  data
-                    .filter(dish => dish.categoria === category || category === '')  // Filtra los datos por categoría
-                    .filter(dish => !maxPrice || Number(dish.precio) <= Number(maxPrice))  // Filtra los datos por precio máximo
-                    .sort((a, b) => maxPrice ? Number(a.precio) - Number(b.precio) : 0)  // Ordena los datos por precio si se especifica un precio máximo
-                    .map(dish => <Cards key={dish.id} data={dish} />)  // Muestra los datos filtrados y ordenados
+                  <>
+                    {data
+                      .filter(dish => dish.categoria === category || category === '')
+                      .filter(dish => !maxPrice || Number(dish.precio) <= Number(maxPrice))
+                      .sort((a, b) => maxPrice ? Number(a.precio) - Number(b.precio) : 0)
+                      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+                      .map(dish => <Cards key={dish.id} data={dish} />)
+                    }
+                    <Pagination className="mt-4"
+                      count={Math.ceil(data.length / itemsPerPage)}
+                      page={currentPage}
+                      onChange={handleChangePage}
+                      color="primary"
+                    />
+                  </>
                 ) : (
                   <p>No hay datos disponibles.</p>
                 )}
