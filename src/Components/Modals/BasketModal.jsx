5b917d83d6fc29fc33cd8f5fce3b3665516af147
KevinJp21 <kevinjp821@gmail.com>
2024-04-21 16:36:32 -0500

    Se agregó una ventana modal de confirmacion, que aparece al presionar el botón de eliminar todos los items de la canasta

---
+++ src/Components/Modals/BasketModal.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Modal, Box, Fade, Typography, Button, Backdrop } from '@mui/material';
+import './BasketModal.css'
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+};
+
+const BasketModal = ({ open, onClose, onConfirm }) => {
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            closeAfterTransition
+            // Ajustes mediante slots
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+              backdrop: {
+                timeout: 500,
+              },
+            }}
+        >
+            <Fade in={open}>
+                <Box sx={style}>
+                    <div className='HeaderModal'>
+                        <p>¿Seguro?</p>
+                    </div>
+                        <div className="ModalTextContainer">
+                            <span>¿Estás seguro de que deseas eliminar todos los productos de la canasta?</span>
+                        </div>
+                    <div className="btnContainerModal">
+                    <button className='btnCancelDel' onClick={onClose}><span>Cancelar</span></button>
+                        <button className='btnConfirmDel' onClick={onConfirm}><span>Confirmar</span></button>
+                    </div>
+                </Box>
+            </Fade>
+        </Modal>
+    );
+};
+
+export default BasketModal;
--- src/Components/NavBar/BasketContent.jsx
+++ src/Components/NavBar/BasketContent.jsx
@@ -1,8 +1,18 @@
 import React, { useContext } from 'react';
 import { BasketContext } from '../contexts/BasketContext';
-
+import BasketModal from '../Modals/BasketModal';
 const BasketContent = () => {
     const { basket, updateQuantity, removeFromBasket, removeAllFromBasket } = useContext(BasketContext);
+    const [isModalOpen, setModalOpen] = React.useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleRemoveAll = () => {
+        setModalOpen(false);
+        removeAllFromBasket();
+    };
 
     if (basket.length === 0) {
         return(
@@ -67,9 +77,10 @@ const BasketContent = () => {
             {/*Se verifica si el numero de items es mayor a cero, se muestra el botón de comprar*/}
             {basket.length > 0 && (
                 <div className="buyContainer" id="buyContainer">
-                    <button className="delAllProducts" id="delAllProducts" onClick={removeAllFromBasket}>
+                    <button className="delAllProducts" id="delAllProducts" onClick={handleOpenModal}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill="#6A696E" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM9 9h6c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1H9c-.55 0-1-.45-1-1v-8c0-.55.45-1 1-1zm6.5-5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1h-2.5z"></path></svg>
                     </button>
+                    <BasketModal open={isModalOpen} onConfirm={handleRemoveAll} onClose={() => setModalOpen(false)} />
                     <button className="btnBuyProducts" id="btnBuyProducts">
                         <div className="btnContent">
                             <p className="btnTitle">Ir a pagar</p>
